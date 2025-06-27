970920e2ce19c77dae2d2f8b4c4ce76e21848e31
Christopher Sanchez <diegsanchez27@gmail.com>
2025-06-27 14:48:25 -0600

    Funciones de productos terminado, falta revision

--- js/Administrator.js
+++ js/Administrator.js
@@ -37,30 +37,28 @@ function MostrarDatos(datos){
 }
 CargarProductos();
 
-async function SubirImagen(file) {
-    const fd = new FormData();
-    fd.append('image', file); 
-    const res = await fetch(API_IMG_URL, {method: 'POST', body: fd}); 
-    const obj = await res.json();
-    return obj.data.url;
-    
-}
 
-const modal1 = document.getElementById("md-agregarProducto");
-const btnAgregar = document.getElementById("btnGuardar");
+
+const modalAgregar = document.getElementById("md-agregarProducto");
+const btnAgregar = document.getElementById("BtnAgregar");
+
 const btnCerrar = document.getElementById("BtnCerrar");
 
 btnAgregar.addEventListener("click", () => {
-    modal1.showModal()
+    modalAgregar.showModal()
 });
 
 btnCerrar.addEventListener("click", () => {
-    modal1.close();
+    modalAgregar.close();
 });
 
-document.getElementById("frmAgregar").addEventListener("submit", async e => {
+
+
+
+document.getElementById("frmAgregarProductos").addEventListener("submit", async e => {
     e.preventDefault();
 
+        const imagen = document.getElementById("imagen-file").value.trim();
         const descripcion = document.getElementById("txtDescripcion").value.trim();
         const estado = document.getElementById("txtAEstado").value.trim();
         const stock = document.getElementById("txtStock").value.trim();
@@ -71,6 +69,112 @@ document.getElementById("frmAgregar").addEventListener("submit", async e => {
         alert("Por favor, complete todos los campos.");
     }
     
-    const respuesta = await fetch(API_URL)
+    const respuesta = await fetch(API_URL, {
+       method: "POST",
+       headers: {'Content-Type':'application/json'},
+       body: JSON.stringify({descripcion, estado, stock, precio, imagen})
+
+    });;
+
+    if(respuesta.ok){
+        alert("El producto se añadio correctamente");
+
+        document.getElementById("frmAgregarProductos").reset();
+
+        modalAgregar.close();
+
+        CargarProductos();
+
+    }else{
+        alert("El Registro no se añadio!");
+    }
 
 });
+
+
+
+async function eliminarProducto(id){
+    const confirmacion = confirm("Quieres eliminar este producto?")
+
+    if(confirmacion){
+        await fetch(`${API_URL}/${id}`, {
+            method: "DELETE"
+        })
+        alert("El registro fue eliminado");
+    }
+    CargarProductos();
+}
+
+
+
+async function SubirImagen(file) {
+    const fd = new FormData();
+    fd.append('image', file); 
+    const res = await fetch(API_IMG_URL, {method: 'POST', body: fd}); 
+    const obj = await res.json();
+    return obj.data.url;
+    
+}
+
+const modalEditar = document.getElementById("md-EditarProducto");
+const modalCerrarEditar = document.getElementById("BtnCerrarEditar");
+
+modalCerrarEditar.addEventListener("click", () => {
+    modalCerrarEditar.close();
+});
+
+function AbrirModalEditar(id, descripcion, estado, stock, precio){
+    document.getElementById("txtIdEditar").value = id;
+    document.getElementById("txtDescProductoED").value = descripcion;
+    document.getElementById("txtAEstadoED").value = estado;
+    document.getElementById("txtStockED").value = stock;
+    document.getElementById("txtPrecioED").value = precio
+
+    modalEditar.showModal();
+}
+
+document.getElementById("frmEditarProducto").addEventListener("submit", async e => {
+    e.preventDefault();
+
+    const id = document.getElementById("txtIdEditar").value;
+    const descripcion = document.getElementById("txtDescProductoED").value;
+    const estado = document.getElementById("txtAEstadoED").value;
+    const stock = document.getElementById("txtStockED").value;
+    const precio = document.getElementById("txtPrecioED").value;
+
+    if(!id || !descripcion || !estado || !stock || !precio){
+        alert("Primero complete los campos");
+        return;
+    }
+
+    const respuesta = await fetch(`${API_URL}/${id}}`, {
+        method: "PUT",
+        headers: {"Content-Type":"applicatinon/json"},
+        body: JSON.stringify({descripcion, estado, stock, precio})
+    });
+
+    if(respuesta.ok){
+        alert("El producto ha sido Actualizado!");
+        modalEditar.close;
+        CargarProductos();
+    }else{
+        alert("El registro no se actualizo!");
+    }
+})
+
+
+
+document.getElementById('imagen-file').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+  
+    reader.onload = function(e) {
+      const preview = document.getElementById('preview-img');
+      preview.src = e.target.result;
+      preview.style.display = 'block';
+    }
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  });
